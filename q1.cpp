5dc43d22c08a3691c740524babc0adf4a0c75a85
SUMIT CHOUDHARY <138365881+Ror003@users.noreply.github.com>
2025-03-31 14:28:40 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter 1 no: ";
+    cin>>a;
+    cout<<"Enter 2 no: ";
+    cin>>b;
+    int sum = a + b;
+    cout<<sum;
+
+}
